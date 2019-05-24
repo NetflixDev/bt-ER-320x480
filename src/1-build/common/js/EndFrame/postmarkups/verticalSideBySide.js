44519ce8bef414bf97c497f26ea92b6f9316b832
Derek Miranda <derek.miranda@ff0000.com>
2019-05-23 18:57:08 -0700

    fix: ensures canvas animation on top
    - redist

--- src/1-build/common/js/EndFrame/postmarkups/verticalSideBySide.js
+++ src/1-build/common/js/EndFrame/postmarkups/verticalSideBySide.js
@@ -59,4 +59,9 @@ export default function sideBySidePostMarkup({
 	} else {
 		T.removeChild(T.ratingsBug)
 	}
+
+	// ensure iris renderer is at top of endFrame
+	if (T.iris) {
+		T.appendChild(T.iris.canvas)
+	}
 }
